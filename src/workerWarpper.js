682afa2e2598558e045f1b2272a96ebdd0d4c2d1
William Ge <weix.ge@intel.com>
2012-02-28 19:13:36 +0800

    add comment

--- src/workerWarpper.js
+++ src/workerWarpper.js
@@ -1,6 +1,8 @@
-/* iFeed : Feed parser 
- * Copyright (C)
- * 				 2011 iwege - http://iwege.com
+/**
+ * iFeed Worker Wrapper : Simple Parse Worker Manager;
+ *  
+ * Copyright (C)  2011 iwege - http://iwege.com
+ * 
  * licensed under the MIT (MIT-license.txt)
  */
 (function(exports){
@@ -9,6 +11,8 @@
 		, status = []
 		, max = 10 // set max worker number
 		;
+		
+	// initialize worker and status;
 	function initWorkers(){
 		for (var i=0; i < max; i++) {
 			workers.push(new Worker('../src/iFeed.js'));
@@ -16,26 +20,38 @@
 		}
 	}
 	
+
 	var iFeed = function (xml, callback){
-		list.push(xml);
+		// push the data to cache list;
+		list.push(xml); 
 		findWorker(callback);
 	}
 	
 	var findWorker = function(callback){
 		var index = status.indexOf(0);
-	
-		if (index != -1) {
-			status[index] = 1;
-			workers[index].onmessage = function(evt){
-				callback && callback(evt.data);
-				if (list.length) {
-					status[index] = 1;
-					workers[index].postMessage(list.pop());
-				}
-				status[index] = 0;
+		
+		if (index == -1) return ; // all workers are in working now;
+		
+		// if we find a worker that doesn't do any work,
+		// make it works;
+		status[index] = 1; 
+		workers[index].onmessage = function(evt){
+			// if worker finished his job, run callback function;
+			callback && callback(evt.data);
+			
+			// check the cache if it isn't empty, 
+			// worker will pick the data and work again.
+			// They are the best workers ! @.@
+			if (list.length) {
+				status[index] = 1;
+				workers[index].postMessage(list.pop());
 			}
-			workers[index].postMessage(list.pop());
+			status[index] = 0;
 		}
+		
+		// inform the worker to work ; 
+		workers[index].postMessage(list.pop());
+		
 	};
 	
 	initWorkers();
